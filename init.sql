779117395cd7a7ac6e3f376c2d7d7aba59d627ee
Mauricio-100 <ceoseshell@gmail.com>
2025-11-03 17:57:09 +0100

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,11 +1,11 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   email TEXT PRIMARY KEY,
   password TEXT NOT NULL,
   otp TEXT,
   verified BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE packages (
+CREATE TABLE IF NOT EXISTS packages (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   version TEXT NOT NULL,
