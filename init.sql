dd267f4df7226b58133c67bec99aa9f8c088e60f
Mauricio-100 <ceoseshell@gmail.com>
2025-11-03 17:50:04 +0100

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+  email TEXT PRIMARY KEY,
+  password TEXT NOT NULL,
+  otp TEXT,
+  verified BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE packages (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  version TEXT NOT NULL,
+  source TEXT,
+  metadata JSONB,
+  downloads INTEGER DEFAULT 0
+);
